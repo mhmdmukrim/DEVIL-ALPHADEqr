cd9f7c4c2a8bd091ba0e8486286812edd1842ad0
DEVIL-ALPHA <92577442+mhmdmukrim@users.noreply.github.com>
2021-12-03 04:37:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Qr Code Genarater For DEVIL ALPHA 
+// I AM MHMD MUKRIM
+// https://wa.me/94766395983
+// 🇱🇰 Sri Lanka
+let {GenarateQrForDEVILALPHA} = require('DEVIL-ALPHA-web-update/det')
+async function DEVIL-ALPHA() {
+await GenarateQrForDEVIL-ALPHA()
+}
+DEVIL-ALPHA()
